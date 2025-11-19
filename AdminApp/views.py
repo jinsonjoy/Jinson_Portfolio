e121b1ac4d6c0cdfd6a7adfcd134f69291f0a669
jinsonjoy <jinsonjoy0001@gmail.com>
2025-11-19 17:01:31 +0530

    Resume added

--- AdminApp/views.py
+++ AdminApp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def index_page(request):
-    return render(request,"Index_Page.html")
+    return render(request,"index.html")
 
 
 
