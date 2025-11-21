05a1382ac1624c76645cc1943843d20ac30084b2
jinsonjoy <jinsonjoy0001@gmail.com>
2025-11-21 14:34:27 +0530

    idex url changed

--- AdminApp/urls.py
+++ AdminApp/urls.py
@@ -3,7 +3,7 @@ from AdminApp import views
 # from AdminApp.views import send_email_view
 
 urlpatterns = [
-    path('',views.index_page,name="index_page"),
+    path('index_page/',views.index_page,name="index_page"),
     # path('send-email/', send_email_view, name='send_email'),
 
 ]
