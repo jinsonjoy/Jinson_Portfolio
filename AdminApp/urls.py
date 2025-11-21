27d2e5f898c73552937016b08043eea5709eed66
jinsonjoy <jinsonjoy0001@gmail.com>
2025-11-21 14:32:25 +0530

    email re added

--- AdminApp/urls.py
+++ AdminApp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from AdminApp import views
-from AdminApp.views import send_email_view
+# from AdminApp.views import send_email_view
 
 urlpatterns = [
     path('',views.index_page,name="index_page"),
-    path('send-email/', send_email_view, name='send_email'),
+    # path('send-email/', send_email_view, name='send_email'),
 
 ]
